766a6cce3e625df2a5dec86daabe57f13179670d
Yangjianxiao0203 <112916147+Yangjianxiao0203@users.noreply.github.com>
2022-10-07 17:25:08 -0400

    Add files via upload

---
+++ tweety_test.py
@@ -0,0 +1,12 @@
+import tweepy
+import config.config_twitter as ct
+client=tweepy.Client(bearer_token=ct.BEARER_TOKEN)
+
+query='covid -is:retweet'
+
+response=client.search_recent_tweets(query=query,max_results=11)
+
+for data in response.data:
+    print(data.id)
+    print(data)
+
